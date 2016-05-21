4d3f7b7890edae89389808ec2432d2bb045b0dd6
Hynaro <acalvo085@gmail.com>
2016-05-21 08:18:35 +0200

    srcbis
    srcbis

--- quelmenteur/src/dwar/Deputes.java
+++ quelmenteur/src/dwar/Deputes.java
@@ -1,5 +1,57 @@
 package dwar;
 
+import java.util.ArrayList;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType=IdentityType.APPLICATION)
+
 public class Deputes {
 
+	@PrimaryKey
+	@Persistent(valueStrategy=IdGeneratorStrategy.IDENTITY)
+	Long id;
+	@Persistent
+	String nom;
+	@Persistent
+	String prenom;
+	@Persistent
+	String parti;
+	@Persistent
+	ArrayList<String> listeVotes;
+	public ArrayList<String> getListeVotes() {
+		return listeVotes;
+	}
+	public void setListeVotes(ArrayList<String> listeVotes) {
+		this.listeVotes = listeVotes;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getParti() {
+		return parti;
+	}
+	public void setParti(String parti) {
+		this.parti = parti;
+	}
+	
 }
--- quelmenteur/src/dwar/Loi.java
+++ quelmenteur/src/dwar/Loi.java
@@ -1,5 +1,26 @@
 package dwar;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType=IdentityType.APPLICATION)
+
 public class Loi {
 
+	@PrimaryKey
+	@Persistent(valueStrategy=IdGeneratorStrategy.IDENTITY)
+	Long id;
+	@Persistent
+	String nom;
+	@Persistent
+	String description;
+	public Long getId() {return id;}
+	public void setId(Long id) {this.id = id;}
+	public String getNom() {return nom;}
+	public void setNom(String nom) {this.nom = nom;}
+	public String getDescription() {return description;}
+	public void setDescription(String description) {this.description = description;}
 }
